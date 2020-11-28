8f81e3f8712a1836fdcd70fde088426f1e224e63
anees <aneesurrehman423@gmail.com>
2020-11-28 16:28:59 +0530

    added authentication backend"

--- PROJECT/main.py
+++ PROJECT/main.py
@@ -1,10 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,session,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_user,logout_user,login_manager,LoginManager
+from flask_login import login_required,current_user
 
 # MY db connection
 local_server= True
 app = Flask(__name__)
 app.secret_key='aneesrehmankhan'
+
+
+# this is for getting unique user access
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
 
 # app.config['SQLALCHEMY_DATABASE_URL']='mysql://username:password@localhost/databas_table_name'
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/hms'
@@ -17,6 +33,14 @@ class Test(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(100))
     email=db.Column(db.String(100))
+
+class User(UserMixin,db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(50))
+    email=db.Column(db.String(50),unique=True)
+    password=db.Column(db.String(1000))
+
+
 
 
 # here we will pass endpoints and run the fuction
@@ -37,19 +61,63 @@ def patient():
 
 @app.route('/bookings')
 def bookings():
+    if not User.is_authenticated:
+        return render_template('login.html')
+    else:
+
+        return render_template('booking.html',username=current_user.username)
     return render_template('booking.html')
 
-@app.route('/signup')
+@app.route('/signup',methods=['POST','GET'])
 def signup():
+    if request.method == "POST":
+        username=request.form.get('username')
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=User.query.filter_by(email=email).first()
+        if user:
+            print("email already exists")
+            return render_template('/signup.html')
+        encpassword=generate_password_hash(password)
+
+        new_user=db.engine.execute(f"INSERT INTO `user` (`username`,`email`,`password`) VALUES ('{username}','{email}','{encpassword}')")
+
+        # this is method 2 to save data in db
+        # newuser=User(username=username,email=email,password=encpassword)
+        # db.session.add(newuser)
+        # db.session.commit()
+
+        return render_template('login.html')
+
+          
+
     return render_template('signup.html')
 
-@app.route('/login')
+@app.route('/login',methods=['POST','GET'])
 def login():
+    if request.method == "POST":
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password,password):
+            login_user(user)
+            return redirect(url_for('bookings'))
+        else:
+            print("invalid credentials")
+            return render_template('login.html')    
+
+
+
+
+
     return render_template('login.html')
 
 @app.route('/logout')
+@login_required
 def logout():
-    return render_template('login.html')
+    logout_user()
+    return redirect(url_for('login'))
 
 
 
@@ -65,3 +133,6 @@ def test():
 
 
 app.run(debug=True)    
+
+
+# username=current_user.username
